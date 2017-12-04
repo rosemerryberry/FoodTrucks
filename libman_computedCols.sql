5e79c2db11cf2b6be7f6d93604a520a104096768
Jessica Libman <jlibman@gmail.com>
2017-12-04 12:13:30 -0800

    added go to cols

--- libman_computedCols.sql
+++ libman_computedCols.sql
@@ -1,26 +1,27 @@
 -- employee age
+go
 CREATE FUNCTION fn_employeeAge(@EmployeeID int)
 RETURNS int
 AS
 BEGIN
-DECLARE @ret = (
+DECLARE @ret int = (
     SELECT DateDiff(
         year, 
         (SELECT DOB FROM tblEmployee WHERE EmployeeID = @EmployeeID), 
         GETDATE()))
 RETURN @ret
 END
-
+go
 ALTER TABLE tblEmployee
 ADD EmployeeAge
 AS dbo.fn_employeeAge(EmployeeID)
-
+go
 -- num employees per truck
 CREATE FUNCTION fn_employeePerTruck(@TruckID int)
 RETURNS int
 AS
 BEGIN
-DECLARE @ret = (
+DECLARE @ret int = (
     SELECT count(*) 
     FROM tblTruck t 
     JOIN tblEmployee e ON e.TruckID = t.TruckID
@@ -28,7 +29,7 @@ DECLARE @ret = (
 )
 RETURN @ret
 END
-
+go
 ALTER TABLE tblTruck
 ADD NumEmployees
 AS dbo.fn_employeePerTruck(TruckID)
